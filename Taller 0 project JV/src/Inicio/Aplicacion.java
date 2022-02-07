3c01c1d724d0ff35e26784abf060cdf02da18a26
d.burgos <david.burgosmendez@gmail.com>
2022-02-06 22:02:11 -0500

    Inicio de App y Restaurante

--- Taller 0 project JV/src/Inicio/Aplicacion.java
+++ Taller 0 project JV/src/Inicio/Aplicacion.java
@@ -5,7 +5,20 @@ public class Aplicacion {
 	public static void main(String[] args) 
 	{
 	
+		
+		
+	}
+	
+	public void mostarMenu() 
+	{
+		
 		System.out.println("Hola, mundo!");
+		
+	}
+	
+	public void ejecutarOpcion(int opcionSel) {
+		
+		
 		
 	}
 	
