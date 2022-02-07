df4f7fd55d29a0b80dd84e430da9c97fc71e38ec
Wyo Chu <w.chu@uniandes.edu.co>
2022-02-06 22:27:46 -0500

    Clase REstaurante basico con sus metodos

--- Taller 0 project JV/src/Inicio/Restaurante.java
+++ Taller 0 project JV/src/Inicio/Restaurante.java
@@ -1,7 +1,86 @@
 package Inicio;
 
+import java.io.File;
+
 public class Restaurante {
 	
+	public static void main(String[] args) 
+	{
+	
+		
+		
+	}
+	
+	public void iniciarPedido(String nombreCLiente, String direccionCliente) 
+	{
+		
+		System.out.println("Iniciando pedido");
+		
+	}
+	
+	public void cerrarYGuardarPedido() 
+	{
+		
+		System.out.println("Guarda Pedido");
+		
+	}
+	
+	public String getPedidoEnCurso() 
+	{
+		String pedido = "getPedidoEnCurso";
+		System.out.println("getPedidoEnCurso");
+		return pedido;
+		
+	}
+	
+	public String getMenuBase() 
+	{
+		String menuBase = "menuBase";
+		System.out.println("menuBaseo");
+		return menuBase;
+		
+	}
+	
+	public String getIngredientes() 
+	{
+		String Ingredientes = "Ingredientes";
+		System.out.println("Ingredientes");
+		return Ingredientes;
+		
+	}
+	
+	
+	public void cargarInformacionRestaurante(File archivoIngredientes, File archivoMenu, File archivoCombos) 
+	{
+		
+		System.out.println("cargarInformacionRestaurante");
+		
+		
+	}
+	
+	public void cargarIngredientese(File archivoIngredientes) 
+	{
+		
+		System.out.println("archivoIngredientes");
+		
+		
+	}
+	
+	public void cargarMenu(File archivoMenu) 
+	{
+		
+		System.out.println("archivoMenu");
+		
+		
+	}
+	
+	public void cargarCombos(File archivoCombos) 
+	{
+		
+		System.out.println("archivoCombos");
+		
+		
+	}
 	
 	
 }
